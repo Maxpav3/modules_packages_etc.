4f04417170d5d88e6c8fc5163bcfe16043f79367
Maxim <maxpavlenko3@gmail.com>
2025-12-19 16:05:23 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from application.db.people import get_employees
+from application.salary import calculate_salary
+from datetime import datetime
+
+print(datetime.now())
+if __name__ == "__main__":
+    get_employees()
+    calculate_salary()  
